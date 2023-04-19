6176b31c2d54a88c814e0b0a13e0a19ce532f89c
roshan <roshanprusty100@gmail.com>
2023-04-19 22:47:08 +0530

    dotenv

--- 6Command-Line-Apps/dotenv.js
+++ 6Command-Line-Apps/dotenv.js
@@ -1,4 +1,15 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 import * as dotenv from 'dotenv'
-dotenv.config()
+if(process.env.Node_ENV){
+    dotenv.config({
+        path:`${__dirname}/.env${process.env.NODE_ENV}`
+    });
+} else{
+    dotenv.config();
+}
+console.log(`${__dirname}/.env${process.env.NODE_ENV}`);
 console.log(process.env.MY_VAR);
 console.log(process.env.NUMBER);
