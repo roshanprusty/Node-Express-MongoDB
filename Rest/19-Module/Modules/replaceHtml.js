514b3eceb9013396caeae1afa044e40cce823596
roshan <roshanprusty100@gmail.com>
2023-05-29 02:26:05 +0530

    -

---
+++ Rest/19-Module/Modules/replaceHtml.js
@@ -0,0 +1,13 @@
+module.export=function(template, product){
+    let output = template.replace('{{%IMAGE%}}', product.productImage);
+    output = output.replace('{{%NAME%}}', product.name);
+    output = output.replace('{{%MODELNAME%}}', product.modeName);
+    output = output.replace('{{%MODELNUMBER%}}', product.modelNumber);
+    output = output.replace('{{%SIZE%}}', product.size);
+    output = output.replace('{{%CAMERA%}}', product.camera);
+    output = output.replace('{{%PRICE%}}', product.price);
+    output = output.replace('{{%COLOR%}}', product.color);
+    output = output.replace('{{%ID%}}', product.id);
+
+    return output;
+}
