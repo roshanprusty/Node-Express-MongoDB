9f69ef718f4e537213ee77e71db16c9bb3894565
roshan <roshanprusty100@gmail.com>
2023-06-10 00:39:18 +0530

    Express - APIs

--- Express/API/handlingRoute.js
+++ Express/API/handlingRoute.js
@@ -5,13 +5,10 @@ import fs from 'fs';
 let app = express();
 let movies = JSON.parse(fs.readFileSync('./data/movies.json'));
 
-/* `app.use(express.json());` is setting up middleware in the Express application to parse incoming
-requests with JSON payloads. This middleware adds a `body` object to the request object, which
-contains the parsed JSON data. This allows the application to handle JSON data in requests easily. */
+
 app.use(express.json());
 
 app.get('/api/v1/movies', (req, res)=>{
-    //here i have written .json just because to have O/P in json format if would have written in .send then it would have in text format
     res.status(200).json({
         status: "success",
         data: {
@@ -32,7 +29,6 @@ app.post('/api/v1/movies', (req,res)=>{
             }
         })
     })
-    // res.send("CREATED");
 })
 const port = 3000;
 app.listen(port, () => {
