84f7be8dbcf2adc48c61e715371749e90ef6d790
roshan <roshanprusty100@gmail.com>
2023-06-20 00:27:47 +0530

    morgan

--- Express/2middleware/app.js
+++ Express/2middleware/app.js
@@ -5,12 +5,23 @@ import fs from 'fs';
 let app = express();
 let movies = JSON.parse(fs.readFileSync('./data/movies.json'));
 
+function logger(req, res, next){
+    console.log("custom middleware created");
+    next();
+}
 
 app.use(express.json());
+app.use(logger);
+app.use((req, res, next) => {
+    req.requestedAt = new Date().toISOString();
+    next();
+})
 
 app.get('/api/v1/movies', (req, res) => {
     res.status(200).json({
         status: "success",
+        requestedAt: req.requestedAt,
+        count: movies.length,
         data: {
             movie: movies
         }
@@ -70,7 +81,7 @@ app.post('/api/v1/movies', (req, res) => {
 app.patch('/api/v1/movies/:id', (req, res) => {
     const id = req.params.id * 1;
     let movieToUpdate = movies.find(el => el.id === id);
-    // console.log(movieToUpdate);
+    console.log(movieToUpdate);
     if (!movieToUpdate) {
         // If movie is not found, return an error response
         return res.status(404).json({
